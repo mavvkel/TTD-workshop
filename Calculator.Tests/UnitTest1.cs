6454d51cf9b0cbbbfdcd7488cfbbdfcc3b02f68a
mavvkel <mic.kusmierz@gmail.com>
2023-03-09 21:25:34 +0100

    Final

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -13,7 +13,7 @@ public class Tests
 
     [TestCase("1,2")]
     [TestCase("2,3")]
-    public void Test1(string value)
+    public void TwoSummands(string value)
     {
         // Given
         int a = int.Parse(value.Split(',')[0]);
@@ -26,17 +26,145 @@ public class Tests
         Assert.That(result, Is.EqualTo(a + b));
     }
 
-    [TestCase("1")]
-    [TestCase("123546")]
-    public void Test2(string value)
+    [Test]
+    public void ManySummandsTest()
     {
         // Given
-        int a = int.Parse(value);
-        
+        string numbers = "1,2,3,4,5,6";
+
         // When
-        int result = _calculator.Add(value);
+        int result = _calculator.Add(numbers);
 
         // Then
-        Assert.That(result, Is.EqualTo(a));
+        Assert.That(result, Is.EqualTo(21));
+    }
+
+    [Test]
+    public void SingleSummandTest()
+    {
+        // Given
+        string numbers = "33";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(33));
+    }
+
+    [Test]
+    public void EmptyStringTest()
+    {
+        // Given
+        string numbers = "";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void NewlineAsSeparatorTest()
+    {
+        // Given
+        string numbers = "4\n9,6";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(19));
+        
+
+    }
+
+    [Test]
+    public void CustomSeparatorTest()
+    {
+        // Given
+        string numbers = "//;;\n1;;2;;3";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(6));
+    }
+
+    [Test]
+    public void OneNegativeTest()
+    {
+        // Given
+        string numbers = "-1,2,3";
+
+        // When
+        // Then
+        var exc = Assert.Throws<NegativeArgumentsException>(() => _calculator.Add(numbers));
+        Assert.That(exc.Message, Is.EqualTo("Negative arguments provided: -1"));
+    }
+
+    [Test]
+    public void MultipleNegativesTest()
+    {
+        // Given
+        string numbers = "-1,2,13,-48,0,-2";
+
+        // When
+        // Then
+        var exc = Assert.Throws<NegativeArgumentsException>(() => _calculator.Add(numbers));
+        Assert.That(exc.Message, Is.EqualTo("Negative arguments provided: -1 -48 -2"));
+    }
+
+    [Test]
+    public void MultipleNegativesAndCustomDelimiterTest()
+    {
+        // Given
+        string numbers = "//;;\n7;;2;;-13;;-1111;;0;;-0";
+
+        // When
+        // Then
+        var exc = Assert.Throws<NegativeArgumentsException>(() => _calculator.Add(numbers));
+        Assert.That(exc.Message, Is.EqualTo("Negative arguments provided: -13 -1111"));
+    }
+
+    [Test]
+    public void MultipleCustomDelimitersTest()
+    {
+        // Given
+        string numbers = "//[*][;;][$]\n7;;2$1;;2*0*4$1";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(17));
+    }
+
+    [Test]
+    public void NumbersOver1000WithMultipleCustomDelimitersTest()
+    {
+        // Given
+        string numbers = "//[(][delim]\n7(2(1delim1001delim999(4delim1";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(1014));
+    }
+
+    [Test]
+    public void SingleNumberOver1000Test()
+    {
+        // Given
+        string numbers = "1001";
+
+        // When
+        int result = _calculator.Add(numbers);
+
+        // Then
+        Assert.That(result, Is.EqualTo(0));
     }
 }
--- TTD_workshop_1/Program.cs
+++ TTD_workshop_1/Program.cs
@@ -4,15 +4,72 @@
     {
         public int Add(string numbers)
         {
-            int a = int.Parse(numbers.Split(',')[0]);
-            int b = int.Parse(numbers.Split(',')[1]);
+            int result = 0;
+            if (!String.IsNullOrEmpty(numbers))
+            {
+                List<string> delimiters = new List<string>();
+                List<string> summands = new List<string>();
 
-            return a + b;
+                // parsing custom delimiter setting string
+                if (numbers.StartsWith("//"))
+                {
+                    numbers = numbers.Remove(0, 2);
+                    // parsing multiple custom delimiters
+                    if(numbers.ElementAt(0) == '[')
+                    {
+
+                        while(numbers.ElementAt(0) == '[')
+                        {
+                            numbers = numbers.Remove(0, 1);
+                            delimiters.Add(numbers.Substring(0, numbers.IndexOf(']')));
+                            numbers = numbers.Remove(0, numbers.IndexOf(']') + 1);
+                        }
+
+                        // remove '\n' after the setting delimiters line
+                        numbers = numbers.Remove(0, 1);
+
+                    }
+                    else
+                    {
+                        delimiters.Add(numbers.Substring(0, numbers.IndexOf('\n')));
+                        numbers = numbers.Remove(0, numbers.IndexOf('\n') + 1);
+                    }
+
+                    summands = numbers.Split(delimiters.ToArray(), StringSplitOptions.None).Where(summand => int.Parse(summand) <= 1000).ToList();
+                }
+                else
+                {
+                    summands = numbers.Split(new char[] { ',', '\n' }).Where(summand => int.Parse(summand) <= 1000).ToList();
+                }
+
+                foreach (var summand in summands)
+                {
+                    if (int.Parse(summand) < 0)
+                        throw new NegativeArgumentsException(summands.Where(summand => int.Parse(summand) < 0).Select(int.Parse).ToArray());
+                    result += int.Parse(summand);
+
+                }
+            }
+            return result;
         }
 
-        public static void Main(String[] args)
+        public static void Main(String[] args) { }
+    }
+
+    public class NegativeArgumentsException : Exception
+    {
+        public NegativeArgumentsException() { }
+
+        public NegativeArgumentsException(int[] negativeArgs) : base(CreateMessage(negativeArgs)) { }
+
+        private static string CreateMessage(int[] negativeArgs)
         {
-            //Calculator c = new Calculator();
+            string message = "Negative arguments provided:";
+            foreach(int arg in negativeArgs)
+            {
+                message = message + " " + arg.ToString();
+            }
+            return message;
         }
     }
 }
